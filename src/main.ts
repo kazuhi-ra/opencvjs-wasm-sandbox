8b07ae54c6393a721f88b372612e46fd4540fd57
kazuhi-ra <kazuhira4126@gmail.com>
2023-02-12 22:20:34 +0900

    サンプルコードが動いた

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log('yo')
+import { cv } from 'opencv-wasm'
+
+const mat = cv.matFromArray(2, 3, cv.CV_8UC1, [1, 2, 3, 4, 5, 6])
+console.log('cols =', mat.cols, '; rows =', mat.rows)
+console.log(mat.data8S)
+
+cv.transpose(mat, mat)
+console.log('cols =', mat.cols, '; rows =', mat.rows)
+console.log(mat.data8S)
